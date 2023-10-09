e29f2775f5dc41f2d3f17dca71124d0aa45b52a0
enaguero <enaguero@gmail.com>
2023-10-09 07:15:01 +0000

    Example how to getcrud people in flask alchemy

--- src/app.py
+++ src/app.py
@@ -8,8 +8,10 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, People
 #from models import Person
+import requests
+import json 
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
@@ -36,6 +38,25 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
+
+@app.route('/import_people')
+def import_people():
+    res = requests.get('https://www.swapi.tech/api/people/')
+    response = json.loads(res.text)
+    
+    data = response["results"]
+    for person_data in data:
+        person_res = requests.get(person_data["url"])
+        person_json = json.loads(person_res.text)
+        result = person_json['result']
+        properties = result['properties']
+        p = People(external_uid = result['uid'], name = properties['name'], birth_year= properties['birth_year'])
+        db.session.add(p)
+
+    db.session.commit()
+
+    return jsonify({"msg": "All the people was added"}), 200
+
 @app.route('/user', methods=['GET'])
 def handle_hello():
 
@@ -44,6 +65,28 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+@app.route('/people', methods=['GET'])
+def handle_people():
+    people = People.query.all()
+    # https://stackoverflow.com/questions/7102754/jsonify-a-sqlalchemy-result-set-in-flask
+    return jsonify([p.serialize() for p in people]), 200
+
+@app.route('/people/<int:person_id>', methods=['PUT', 'GET'])
+def handle_person(person_id):
+    # Potential reference : https://stackoverflow.com/questions/54472696/failed-to-decode-json-object-expecting-value-line-1-column-1-char-0
+    if request.method == 'GET':
+        person = People.query.get(person_id)
+        return jsonify(person.serialize()), 200
+    
+    if request.method == 'PUT':
+        person = People.query.get(person_id)
+        body = request.get_json()
+        person.username = body.username
+        db.session.commit()
+        return jsonify(person.serialize()), 200
+    
+    return "Invalid Method", 404
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -5,8 +5,6 @@ db = SQLAlchemy()
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
 
     def __repr__(self):
         return '<User %r>' % self.username
@@ -14,6 +12,22 @@ class User(db.Model):
     def serialize(self):
         return {
             "id": self.id,
-            "email": self.email,
+            "email": self.email
+        }
+
+class People(db.Model):
+    # Reference: https://flask-sqlalchemy.palletsprojects.com/en/2.x/models/
+    id = db.Column(db.Integer, primary_key=True)
+    external_uid = db.Column(db.String(120), nullable=False)
+    name = db.Column(db.String(120), nullable=True)
+    birth_year = db.Column(db.String(120), nullable=True)
+
+    def __repr__(self):
+        return '<People %r>' % self.username
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "uid": self.external_uid,
             # do not serialize the password, its a security breach
         }
